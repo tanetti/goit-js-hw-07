3b59c91e87439005e7bd1397f56bf86b291d7361
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-17 18:45:17 +0300

    Add solution for task one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('div.gallery');
+
+const createGalleryMarkup = galleryData =>
+  galleryData
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+    )
+    .join('');
+
+galleryContainerRef.innerHTML = createGalleryMarkup(galleryItems);
+
+const showLightboxModal = imageURL => {
+  const modal = basicLightbox.create(`<img src="${imageURL}" width="800" height="600">`);
+
+  modal.show();
+};
+
+const onGalleryContainerClick = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') return;
+
+  showLightboxModal(event.target.dataset.source);
+};
+
+galleryContainerRef.addEventListener('click', onGalleryContainerClick);
