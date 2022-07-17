36fbb997102cf58c9a7557da70cfde8d9d1a7aff
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-17 20:57:38 +0300

    Add solution for task two

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('ul.gallery');
+
+const createGalleryMarkup = galleryData =>
+  galleryData
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+
+galleryContainerRef.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('ul.gallery a', {
+  overlayOpacity: 0.85,
+  captionsData: 'alt',
+  captionDelay: 250,
+  disableRightClick: true,
+  alertError: false,
+  maxZoom: 3,
+});
